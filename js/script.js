6af36a9011c57a6c972ead4fa462f6b7c849fc4d
Thiago Pereira <thiagotmb6@gmail.com>
2024-08-12 15:34:51 -0300

    Insert the scroll behavior - Change the navbar background color

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Toggle Mobile menu
+
 const mobileBtn = document.querySelector('.mobile-menu-toggle i');
 const mobileMenuItems = document.querySelector('.mobile-menu-items');
 
@@ -5,3 +7,14 @@ function showMenu() {
   mobileMenuItems.classList.toggle('active');
 }
 mobileBtn.addEventListener('click', showMenu);
+
+// Change Navbar background on scroll
+const navbar = document.querySelector('.navbar');
+function menuScroll() {
+  if (scrollY > 0) {
+    navbar.classList.add('navbar-scroll');
+  } else {
+    navbar.classList.remove('navbar-scroll');
+  }
+}
+window.addEventListener('scroll', menuScroll);
